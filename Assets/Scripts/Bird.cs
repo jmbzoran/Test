210c36564873dc785fbef0bea8522b0bea1955d8
zoran <jmbzoran@gmail.com>
2017-05-20 12:38:26 +0200

    Prvi komit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+
+    public float speed=2;
+
+    public float force = 400;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
+
---
+++ Assets/Scripts/obstacle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class obstacle : MonoBehaviour {
+
+    public float speed=0;
+
+    public float switchTime = 2;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+
+        InvokeRepeating("Switch", 0, switchTime);
+    }
+	
+	// Update is called once per frame
+	void Switch()
+    {
+        GetComponent<Rigidbody2D>().velocity *= -1;
+    }
+}
